bf04aa2f5ee0cfafd248da7fd9d1b979e7845026
Frederic Le Bris <flebris@gmail.com>
2012-07-03 20:22:01 +0200

    Test git

--- src/helloworld.py
+++ src/helloworld.py
@@ -7,7 +7,7 @@ class MainPage(webapp.RequestHandler):
     
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World! Fred')
+        self.response.out.write('Hello, webapp World! Fred git')
 
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
